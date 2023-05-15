84b912749b5fa6750f9ca55399a18268d6148b65
Robert Szalkai <robert.szalkai.work@gmail.com>
2023-05-15 10:22:46 +0300

    added eslint + prettier config + removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import { useState } from 'react';
 import './App.css';
 import Header from '@/Components/Header';
-import ReadTheDocs from '@/Components/ReadTheDocs';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <p>Portofolio {count}</p>
       <Header />
-      <div className="card">
-        <button type="button" onClick={() => setCount(count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <ReadTheDocs />
     </div>
   );
 }
